0ddd7a4d7da6c5ce5ee4ee9a351ab6618d54b399
Vijul Malviya <50556784+VijulMalviya@users.noreply.github.com>
2021-09-16 02:20:12 +0530

    Add files via upload

--- Script/script.js
+++ Script/script.js
@@ -21,3 +21,19 @@ function scrollFunction() {
         document.querySelector(".navigation").classList.remove("shadow");
       }
     }
+
+
+
+    var swiper = new Swiper(".mySwiper", {
+      effect: "cube",
+      grabCursor: true,
+      cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
