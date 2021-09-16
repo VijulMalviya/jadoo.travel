c0ad6bc3314bd440baee62124d44b6f56f9ca880
Vijul Malviya <50556784+VijulMalviya@users.noreply.github.com>
2021-09-16 16:39:59 +0530

    Add files via upload

--- Script/script.js
+++ Script/script.js
@@ -1,39 +1,44 @@
 AOS.init();
 
-$(document).ready(function() {
-    $("#bars").click(function(){
-        $(".nav-links").slideToggle("slow");
-      });
-      $(".servicecard").hover(function(){
-        $(".servicecard").removeClass('activeservice');
-        $(".img").css('display','none');
+$(window).on('load' ,function(){
+  $(".loader").fadeOut("2000");
+  
+})
 
-      });
+$(document).ready(function () {
+  $("#bars").click(function () {
+    $(".nav-links").slideToggle("slow");
+  });
+  $(".servicecard").hover(function () {
+    $(".servicecard").removeClass("activeservice");
+    $(".img").css("display", "none");
+  });
 });
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
+
 function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.querySelector(".navigation").style.backgroundColor = "white";
-        document.querySelector(".navigation").classList.add("shadow");
-      } else {
-        document.querySelector(".navigation").style.backgroundColor = "transparent";
-        document.querySelector(".navigation").classList.remove("shadow");
-      }
-    }
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.querySelector(".navigation").style.backgroundColor = "white";
+    document.querySelector(".navigation").classList.add("shadow");
+  } else {
+    document.querySelector(".navigation").style.backgroundColor = "transparent";
+    document.querySelector(".navigation").classList.remove("shadow");
+  }
+}
 
-
-
-    var swiper = new Swiper(".mySwiper", {
-      effect: "cube",
-      grabCursor: true,
-      cubeEffect: {
-        shadow: true,
-        slideShadows: true,
-        shadowOffset: 20,
-        shadowScale: 0.94,
-      },
-      pagination: {
-        el: ".swiper-pagination",
-      },
-    });
+var swiper = new Swiper(".mySwiper", {
+  effect: "cube",
+  grabCursor: true,
+  cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
